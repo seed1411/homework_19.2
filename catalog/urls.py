ce2bf5576b75378e15e758d1ca29fe14bc54a19b
Anton Gromov <seed14112@yandex.ru>
2024-07-07 12:47:26 +0300

    Корректировка урла 'contacts' и добавление запятой в параметр STATICFILES_DIRS

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from apps import CatalogConfig
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts')
 ]
