5d1e4207efac2046f3f292555e33aee46bfd124f
Anton Gromov <seed14112@yandex.ru>
2024-07-20 11:44:28 +0300

    создание контроллера prod_list и путь в urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, prod_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>', prod_list, name='prod_list')
 ]
