16f370320df09efa894259368cdf38b74d32bb72
Anton Gromov <seed14112@yandex.ru>
2024-07-26 10:29:53 +0300

    Добавление дополнительный функций в модули blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
@@ -7,9 +8,20 @@ from blog.models import Blog
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -17,11 +29,21 @@ class BlogCreateView(CreateView):
     fields = ['title', 'content', 'image', 'is_published']
     success_url = reverse_lazy('blog:blog_views')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+        return super().form_valid((form))
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ['title', 'content', 'image', 'is_published']
     success_url = reverse_lazy('blog:blog_views')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
